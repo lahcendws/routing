7c10a928ce23515cc038ef946e6aa0e4655e6ffb
mister45 <misterdev45@gmail.com>
2022-01-03 15:57:23 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignupComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  exports: [
+    [RouterModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
